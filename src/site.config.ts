ca2e0dbf7b1b7736913e5a8aa85599d93206fd45
Suryansh Patel <patelsuryansh@gmail.com>
2024-04-19 12:57:06 +0530

    changed CG PSC to CGPSC almost everywhere

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Hit360D",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "CG PSC Blog",
+	title: "CGPSC Blog",
 	// Meta property used as the default description meta property
-	description: "Just a simple CG PSC blog.",
+	description: "Just a simple CGPSC blog.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-IN",
 	// Meta property, found in src/components/BaseHead.astro L:42
